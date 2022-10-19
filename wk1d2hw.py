5125fa20e77677e019dd1468dcd2b766f34ae2ab
1919dev <sfbsturrock@gmail.com>
2022-10-19 09:07:45 +0100

    fix name/capital error

--- wk1d2hw.py
+++ wk1d2hw.py
@@ -96,7 +96,7 @@ united_kingdom = [
 ]
 
 # 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
-united_kingdom[1]["name"] = "Cardiff"
+united_kingdom[1]["capital"] = "Cardiff"
 
 # 2. Create a dictionary for Northern Ireland and add it to the `united_kingdom` list (The capital is Belfast, and the population is 1,811,000).
 united_kingdom.append(
