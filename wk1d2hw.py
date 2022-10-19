736390a195113422a1c17236e30a88b546f6bd57
1919dev <sfbsturrock@gmail.com>
2022-10-19 09:09:57 +0100

    fix pop not changed

--- wk1d2hw.py
+++ wk1d2hw.py
@@ -100,7 +100,7 @@ united_kingdom[1]["capital"] = "Cardiff"
 
 # 2. Create a dictionary for Northern Ireland and add it to the `united_kingdom` list (The capital is Belfast, and the population is 1,811,000).
 united_kingdom.append(
-    {"name": "Northern Ireland", "population": 5295000, "capital": "Belfast"}
+    {"name": "Northern Ireland", "population": 1811000, "capital": "Belfast"}
 )
 
 # 3. Use a loop to print the names of all the countries in the UK.
@@ -116,10 +116,6 @@ print(total_pop_uk)
 
 
 # For the following list of numbers:
-
-from operator import le
-from re import L
-from tkinter import N
 
 
 numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
